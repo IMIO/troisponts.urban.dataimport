c62d0aad41c2843c856653383bebb09b4dfc5df5
jjaumotte <julien.jaumotte@imio.be>
2017-01-17 14:39:25 +0100

    Fix some bobtemplate template profile errors

--- src/troisponts/urban/dataimport/settings.py
+++ src/troisponts/urban/dataimport/settings.py
@@ -3,7 +3,7 @@
 from imio.urban.dataimport.browser.controlpanel import ImporterControlPanel
 from imio.urban.dataimport.browser.import_panel import ImporterSettings
 from imio.urban.dataimport.browser.import_panel import ImporterSettingsForm
-from imio.urban.dataimport.agorawin.settings import AgorawinImporterFromImportSettings
+from imio.urban.dataimport.access.settings import AccessImporterFromImportSettings
 
 
 class TroispontsImporterSettingsForm(ImporterSettingsForm):
@@ -19,7 +19,7 @@ class TroispontsImporterControlPanel(ImporterControlPanel):
     import_form = TroispontsImporterSettings
 
 
-class TroispontsImporterFromImportSettings(AgorawinImporterFromImportSettings):
+class TroispontsImporterFromImportSettings(AccessImporterFromImportSettings):
     """ """
 
     def get_importer_settings(self):
